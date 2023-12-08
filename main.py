0d36dccfa0c73d06ec28fb867ee8cc493b7e98b5
Pavel D <p_dolmatov05@mail.ru>
2023-12-08 11:29:09 +0300

    fix mouse grab

--- main.py
+++ main.py
@@ -12,19 +12,23 @@ simulation_started = True
 
 bodies = []
 captured_part = None
+N = 3
 
-blocks.append(Block([[-100, window_height-20], [window_width+100, window_height-20], [window_width+100, window_height+50], [-100, window_height+50]]))
-blocks.append(Block([[-100, 20], [window_width+100, 20], [window_width+100, -50], [-100, -50]]))
-blocks.append(Block([[window_width-20, -30], [window_width+20, -30],[window_width+20, window_height+30], [window_width-20, window_height+30]] ))
-blocks.append(Block([[-100, -30], [20, -30],[20, 630], [-100, 630]] ))
-blocks.append(Block([[300, 600], [500, 400], [500,600]]))
+mouse_pos = np.zeros(2)
 
+blocks.append(Block([[-100, window_height - 20], [window_width + 100, window_height - 20],
+                     [window_width + 100, window_height + 50],[-100, window_height + 50]]))
+blocks.append(Block([[-100, 20], [window_width + 100, 20], [window_width + 100, -50], [-100, -50]]))
+blocks.append(Block([[window_width - 20, -30], [window_width + 20, -30],
+                     [window_width + 20, window_height + 30],[window_width - 20, window_height + 30]]))
+blocks.append(Block([[-100, -30], [20, -30], [20, 630], [-100, 630]]))
+blocks.append(Block([[300, 600], [500, 400], [500, 600]]))
 
-parts = [Particle(np.array([window_width/2, window_height/2]), 5, color="blue", V=np.array([-0., 5.])),
-         Particle(np.array([window_width/2+50, window_height/2]), 5, color="red", V=np.array([0., 0.])),
-         Particle(np.array([window_width/2+50, window_height/2+50]), 5, color="green", V=np.array([0., 0.])),
-         Particle(np.array([window_width/2, window_height/2+50]), 5),
-         Particle(np.array([window_width/2+25, window_height/2+25]), 5)]
+parts = [Particle(np.array([window_width / 2, window_height / 2]), 5, color="blue", V=np.array([-0., 5.])),
+         Particle(np.array([window_width / 2 + 50, window_height / 2]), 5, color="red", V=np.array([0., 0.])),
+         Particle(np.array([window_width / 2 + 50, window_height / 2 + 50]), 5, color="green", V=np.array([0., 0.])),
+         Particle(np.array([window_width / 2, window_height / 2 + 50]), 5),
+         Particle(np.array([window_width / 2 + 25, window_height / 2 + 25]), 5)]
 
 connects = [Connection((parts[0], parts[1])),
             Connection((parts[0], parts[3])),
@@ -34,7 +38,6 @@ connects = [Connection((parts[0], parts[1])),
             Connection((parts[2], parts[3])),
             Connection((parts[2], parts[4])),
             Connection((parts[3], parts[4]))]
-
 
 bodies.append(Body(connects=connects, parts=parts))
 
@@ -66,39 +69,42 @@ def stop_sim():
 
 def simulation():
     for body in bodies:
-        body.update_pos(DT, 1)
+        body.update_pos(DT, 2)
         update_body_image(space, body)
+
+    move_captured_part()
 
     if simulation_started:
         space.after(10, simulation)
 
+
 def mousedown(event):
-    global captured_part, cursor
+    global captured_part
     n = 4
-    #print(event.x, event.y)
-    if captured_part is None:
+    if not captured_part:
         for body in bodies:
             for part in body.parts:
-                if (part.pos[0] - n*part.r <= event.x <= part.pos[0] + n*part.r) and (part.pos[1] - n*part.r <= event.y <= part.pos[1] + n*part.r):
-                    part.captured = True
+                if ((part.pos[0] - n * part.r <= event.x <= part.pos[0] + n * part.r)
+                        and (part.pos[1] - n * part.r <= event.y <= part.pos[1] + n * part.r)):
                     captured_part = part
-                    cursor = Cursor(np.array([event.x, event.y]))
+                    print(captured_part)
+                    break
+
 
 def mouseup(event):
     global captured_part
-    if captured_part is not None:
-        captured_part.captured = False
-        captured_part = None
+    captured_part = None
+
 
 def mousemove(event):
-    global captured_part, cursor
-    
-    if captured_part is not None:
-        cursor.update(np.array([event.x, event.y])) ##change later to dt based on fps rate
-        #calculate_fict_force(captured_part, cursor.a)
-        #print(captured_part.pos)
-        #translate(bodies[0], cursor.pos - cursor.prev_pos)
-        translate(bodies[0], np.array([event.x, event.y]) - captured_part.pos)
+    global mouse_pos
+    mouse_pos = np.array([event.x, event.y])
+
+def move_captured_part():
+    global mouse_pos
+    if captured_part:
+        r_vector = mouse_pos - captured_part.pos
+        captured_part.F += r_vector / np.linalg.norm(r_vector) * min([20 ,(math.exp(np.linalg.norm(r_vector)/15)/10-1)])
 
 def main():
     global start_button
@@ -109,7 +115,7 @@ def main():
     # пространство отображается на холсте типа Canvas
     space = tkinter.Canvas(root, width=window_width, height=window_height, bg="white")
     space.bind('<Button-1>', mousedown)
-    space.bind('<B1-Motion>', mousemove)
+    space.bind('<Motion>', mousemove)
     space.bind('<ButtonRelease-1>', mouseup)
     space.pack(side=tkinter.LEFT)
     # нижняя панель с кнопками
@@ -118,9 +124,8 @@ def main():
 
     start_button = tkinter.Button(frame, text="Start", command=start_sim)
     start_button.pack()
-    #start_button.bind('<Button-1>', start_sim)
-    #start_button.pack(side=tkinter.RIGHT)
-
+    # start_button.bind('<Button-1>', start_sim)
+    # start_button.pack(side=tkinter.RIGHT)
 
     start_sim()
 
